c95f37ccb1950f8f2b8993d1f41cab2e81733888
Alicia Bures <arosebures@gmail.com>
2025-01-20 18:12:33 -0500

    Completed part one and updated Readme

--- js/index.js
+++ js/index.js
@@ -1,3 +1,49 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/* 
+    Create an array and loop through it, logging out each value using the basic loop syntax.
+    Create an array and loop through it, logging out each value using the "for...of" syntax.
+    Create an array and loop through it, logging out each value using the Array.forEach() method.
+    Loop through an array using "for...of" and try assigning all the elements a new value. What happens?
+    Loop through an array using the Array.forEach() method and try assigning all the elements a new value. What happens?
+    Research "for...in" loops.
+*/
+/*
+let numbers = [1, 2, 3, 4, 5, 6]
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}
+*/
+/*
+let grades = [85, 90, 78, 92, 88];
+let sum = 0;
+for (let grade of grades) {
+    sum += grade;
+}
+
+let average = sum / grades.length;
+console.log(average);
+*/
+/*
+let ages = [5, 13, 8, 10];
+let sum = 0;
+for (let age of ages) {
+    sum += age;
+}
+let average = sum / ages.length;
+console.log(average);
+*/
+/*
+const fruits = ['apple', 'banana', 'cherry'];
+fruits.forEach(function(fruit, index) {
+    console.log(index + ': ' + fruit);
+});
+*/
+/*
+const flavors = ["miso", "potato", "cabbage"];
+flavors.forEach(function(flavors, index) {
+    console.log(flavors + " soup")
+});
+*/
+
